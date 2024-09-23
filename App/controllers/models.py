12d916ee974300920a3698025ccecbba2a2e9955
Honcho TheConquistador <jehlani.joseph@my.uwi.edu>
2024-09-23 22:42:09 +0000

    Made more modifications to controllers

--- App/controllers/models.py
+++ App/controllers/models.py
@@ -17,56 +17,12 @@ def sign_up_applicant(fName,lName,username,password):
     db.session.commit()
     return newApplicant
 
-#apply to job 
-
-def apply_to_job(jobID,userID):
-    job = Jobs.query.filter_by(jobID=jobID).first()
-    user = Applicant.query.filter_by(id=userID).first()
-    if not job or not user:
-        print("Error applying to job!!!")
-    newJobApplication = JobApplications(jobID,userID)
-    allJobs = JobApplications.query.filter_by(jobID=jobID,userID=userID).first()
-    if allJobs: #checks to see if the job application exists already
-        print("Job application already exists!!!")
-        return
-    db.session.add(newJobApplication)
-    db.session.commit()
-    return
-
-#create Job 
-
-def create_job(company,jobTitle):
-    job = Jobs(company, jobTitle)
-    jobCheck = Jobs.query.filter_by(company=company,jobTitle=jobTitle).first()
-    if jobCheck:
-        print("Job already exists!!!")
-        return
-    print("Successfully added a job!")
-    db.session.add(job)
-    db.session.commit()
-    return
-
-#view applicants
-
-def get_all_applicants():
-    jobsList = JobApplications.query.all()
-    applicants = [job.get_json() for job in jobsList]
-
-#view applicants by job id 
-def get_all_applicants_by_jobID(id):
-    jobsList = JobApplications.query.filter_by(jobID=id)
-    if not jobs:
-        print("Error!!!")
-        return 
-    applicants = [job.get_json() for job in jobsList]
-    return applicants
-
 #view jobs by company 
 def get_jobs_by_company(companyName):
     companyOfferings = Jobs.query.filter_by(company=companyName)
     if not company:
         print("Error finding company!!!")
-        return
+        return None
     jobsList = [job.get_json() for job in companyOfferings]
     return jobsList
 
@@ -76,3 +32,98 @@ def get_all_jobs():
     jobs = Jobs.query.all() 
     jobsList = [job.get_json() for job in jobs]
     return jobsList
+
+
+
+#view applicants -----------admin-------------
+
+def get_all_applicants(staffID, fName):
+    jobsList = JobApplications.query.all()
+    findAdmin = Admin.query.filter_by(staffID=staffID,fName=fName).first()
+    if not findAdmin:
+        print("Error!!!")
+        return None
+    jobApplications = JobApplications.query.all()
+    if not jobApplications:
+        print(f"No applicants found!!!")
+        return
+
+    print(f"All Job Applicants:")
+    for application in jobApplications:
+        # Get the applicant information
+        applicant = Applicant.query.get(application.userID)
+        if applicant:
+            applicant_info = {
+                'id': applicant.id,
+                'name': f"{applicant.fName} {applicant.lName}"
+            }
+            applicants_list.append(applicant_info)
+        else:
+            print(f"- Unknown applicant (ID: {application.userID})")
+
+    return applicants_list
+
+#view applicants by job id -----------admin--------
+def get_all_applicants_by_jobID(staffID, fName, id):
+    # Verify admin
+    findAdmin = Admin.query.filter_by(staffID=staffID, fName=fName).first()
+    if not findAdmin:
+        print("Error: Invalid admin credentials")
+        return None
+
+    # Get all job applications for the given job ID
+    jobApplications = JobApplications.query.filter_by(jobID=id).all()
+    if not jobApplications:
+        print(f"No applicants found for job ID: {id}")
+        return None
+
+    applicants_list = []
+
+    print(f"Applicants for job ID {id}:")
+    for application in jobApplications:
+        # Get the applicant information
+        applicant = Applicant.query.get(application.userID)
+        if applicant:
+            applicant_info = {
+                'id': applicant.id,
+                'name': f"{applicant.fName} {applicant.lName}"
+            }
+            applicants_list.append(applicant_info)
+            
+        else:
+            print(f"- Unknown applicant (ID: {application.userID})")
+
+    return applicants_list
+
+#create Job ------------------------------admin-------------
+
+def create_job(company,jobTitle, staffID, fName):
+    job = Jobs(company, jobTitle)
+    findAdmin = Admin.query.filter_by(staffID=staffID,fName=fName).first()
+    jobCheck = Jobs.query.filter_by(company=company,jobTitle=jobTitle).first()
+    if jobCheck:
+        print("Job already exists!!!")
+        return
+    if not findAdmin:
+        print("Not a valid administrator!!!")
+        return
+    print("Successfully added a job!")
+    db.session.add(job)
+    db.session.commit()
+    return
+
+#apply to job 
+
+def apply_to_job(jobID,userID):
+    job = Jobs.query.filter_by(jobID=jobID).first()
+    user = Applicant.query.filter_by(id=userID).first()
+    if not job or not user:
+        print("Error applying to job!!!")
+    allJobs = JobApplications.query.filter_by(jobID=jobID,userID=userID).first()
+    newJobApplication = JobApplications(jobID,userID)
+    if allJobs: #checks to see if the job application exists already
+        print("Job application already exists!!!")
+        return
+    db.session.add(newJobApplication)
+    db.session.commit()
+    return
--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,10 @@ from App.database import db, get_migrate
 from App.models import User
 from App.main import create_app
 from App.controllers import ( create_user, get_all_users_json, get_all_users, initialize )
-
+from App.controllers import (
+    create_admin, sign_up_applicant, apply_to_job, create_job, 
+    get_all_applicants, get_all_applicants_by_jobID, get_jobs_by_company,
+    get_all_jobs)
 
 # This commands file allow you to create convenient CLI commands for testing controllers
 
@@ -67,3 +70,76 @@ def user_tests_command(type):
     
 
 app.cli.add_command(test)
+
+
+'''
+Job Board Commands
+'''
+
+jobBoard = AppGroup('job',help="Job Board Commands")
+
+@jobBoard.command("create_admin", help="Adds a new admin to the database")
+@click.argument("fName", default="string")
+@click.argument("lName", default="string")
+def create_admin_command(fName,lName):
+    create_admin(fName,lName)
+    print(f'Admin {fName} added!!!')
+
+
+@jobBoard.command("newapp", help="Adds a new applicant to the database")
+@click.argument("fName", default="string")
+@click.argument("lName", default="string")
+@click.argument("username", default="string")
+@click.argument("password", default="string")
+def sign_up_applicant_command(fName,lName,username,password):
+    sign_up_applicant(fName,lName,username,password)
+    print(f'{fName} {lName} registered!!!')
+
+@jobBoard.command("companyjobs", help="Displays all jobs offered by a company")
+@click.argument("company", default="string")
+def get_jobs_by_company_command(company):
+    jobList = get_jobs_by_company(company)
+    if not jobList:
+        return
+    print(f'Jobs offered by {company}:')
+    for job in jobList:
+        print(job['jobTitle'])
+
+@jobBoard.command("alljobs", help="Displays all jobs offered")
+def display_all_jobs():
+    jobsList = get_all_jobs()
+    print("___JOBS OFFERED___")
+    for i in range(len(jobsList)):
+        print(f'{i} | {jobsList[i]["JobTitle"]} | Company: {jobsList[i]["company"]}')
+
+@jobBoard.command("view_applicants", help="Displays all applicants")
+@click.argument("staffID", default="string")
+@click.argument("fName", default="string")
+def view_all_applicants_command(staffID,fName):
+    appList = get_all_applicants(staffID,fName)
+    if not appList:
+        return
+    print("___APPLICANT LIST___")
+    for i in range(len(appList)):
+        print(f"{i} | ID: {applicant_info[i]['id']} {applicant_info[i]['name']}")
+
+@jobBoard.command("view_app_by_id", help="Displays all applicants when given a job ID")
+@click.argument("staffID", default="string")
+@click.argument("fName", default="string")
+@click.argument("id", default="string")
+def view_all_applicants_by_jobID_command(staffID,fName,id):
+    jobID = int(id)
+    appList = get_all_applicants_by_jobID(staffID,fName,jobID)
+    if not appList:
+        return
+    print("___APPLICANT LIST___")
+    for i in range(len(appList)):
+        print(f"{i} | ID: {applicant_info[i]['id']} {applicant_info[i]['name']}")
+
+
+
+
+
+
+
+app.cli.add_command(jobBoard)
